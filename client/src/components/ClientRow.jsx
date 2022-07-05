5df9e1e010962a993ff273e071fb3a11dde69916
asimimamabdullah <asimimamabdullah@gmail.com>
2022-07-05 15:18:44 +0500

    All Done

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,24 +2,30 @@ import { FaTrash } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
 import { DELETE_CLIENT } from "../mutations/clientMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 
 const ClientRow = ({ client }) => {
 	const [deleteClient] = useMutation(DELETE_CLIENT, {
 		variables: { id: client.id },
-		refetchQueries: [{ query: GET_CLIENTS }], // not for too much load
-		update(cache, { data: { deleteClient } }) {
-			const { clients } = cache.readQuery({
-				query: GET_CLIENTS,
-			});
-			cache.writeQuery({
-				query: GET_CLIENTS,
-				data: {
-					clients: clients.filter(
-						(client) => client.id !== deleteClient.id,
-					),
-				},
-			});
-		},
+		refetchQueries: [
+			{ query: GET_CLIENTS },
+			{
+				query: GET_PROJECTS,
+			},
+		], // not for too much load
+		// update(cache, { data: { deleteClient } }) {
+		// 	const { clients } = cache.readQuery({
+		// 		query: GET_CLIENTS,
+		// 	});
+		// 	cache.writeQuery({
+		// 		query: GET_CLIENTS,
+		// 		data: {
+		// 			clients: clients.filter(
+		// 				(client) => client.id !== deleteClient.id,
+		// 			),
+		// 		},
+		// 	});
+		// },
 	});
 
 	return (
